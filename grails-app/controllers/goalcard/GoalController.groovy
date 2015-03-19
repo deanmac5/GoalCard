ff0e3adb46e4acec40fcef719b2d5f69a0a91480
Dean Macaulay <dean@magpies.net>
2015-03-19 21:58:09 +1100

    Added config for logging

--- grails-app/controllers/goalcard/GoalController.groovy
+++ grails-app/controllers/goalcard/GoalController.groovy
@@ -1,6 +1,9 @@
 package goalcard
 
+import org.apache.commons.logging.LogFactory
+
 class GoalController {
+    private static final log = LogFactory.getLog(this)
 
     def index() {
         render "Inside Goal Card controller"
