0bac024052374eda0443a7312c566da2c4853a29
Dean Macaulay <dean@magpies.net>
2015-03-25 07:34:46 +1100

    Goal index has jumbotron - just proving that it works

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -52,7 +52,7 @@ grails.project.dependency.resolution = {
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
 
-//        compile "com.bootstrapcdn.maxcdn:twitter-bootstrap:3.3.4"
+
     }
 
     plugins {
@@ -63,7 +63,7 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:1.9.9"
-        compile ":lesscss-resources:1.3.3"
+//        compile ":lesscss-resources:1.3.3"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
--- grails-app/controllers/goalcard/GoalController.groovy
+++ grails-app/controllers/goalcard/GoalController.groovy
@@ -5,7 +5,9 @@ import org.apache.commons.logging.LogFactory
 class GoalController {
     private static final log = LogFactory.getLog(this)
 
-    def index() {
-        render "Inside Goal Card controller"
-    }
+    static scaffold = true
+
+//    def index() {
+//        render "Inside Goal Card controller"
+//    }
 }
